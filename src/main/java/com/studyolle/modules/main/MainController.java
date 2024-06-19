8af5d2b3357c9a737788b6b2950ca8c3985ceae4
Oniqued <zktl3575@gmail.com>
2024-06-19 19:14:20 +0900

    feat : 알림에 따라 아이콘 변경 기능 추가 (Interceptor 추가)

--- src/main/java/com/studyolle/modules/main/MainController.java
+++ src/main/java/com/studyolle/modules/main/MainController.java
@@ -2,12 +2,15 @@ package com.studyolle.modules.main;
 
 import com.studyolle.modules.account.Account;
 import com.studyolle.modules.account.CurrentUser;
+import com.studyolle.modules.notification.NotificationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainContoller {
+public class MainController {
+
     @GetMapping("/")
     public String home(@CurrentUser Account account, Model model) {
         if (account != null) {
--- src/main/java/com/studyolle/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyolle/modules/notification/NotificationRepository.java
@@ -1,7 +1,9 @@
 package com.studyolle.modules.notification;
 
+import com.studyolle.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+    Long countByAccountAndChecked(Account account, boolean checked);
 }
