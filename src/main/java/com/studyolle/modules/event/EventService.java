3158594fc04eb0864d1d094f4ae677cea2026a23
Oniqued <zktl3575@gmail.com>
2024-06-20 14:34:17 +0900

    feat : 스터디 모임 관련 알림 기능 추가

--- src/main/java/com/studyolle/modules/event/EventService.java
+++ src/main/java/com/studyolle/modules/event/EventService.java
@@ -3,8 +3,10 @@ package com.studyolle.modules.event;
 import com.studyolle.modules.account.Account;
 import com.studyolle.modules.event.form.EventForm;
 import com.studyolle.modules.study.Study;
+import com.studyolle.modules.study.event.StudyUpdateEvent;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,21 +20,25 @@ public class EventService {
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
     private final EnrollmentRepository enrollmentRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Event createEvent(final Event event, final Study study, final Account account) {
         event.setCreatedBy(account);
         event.setCreatedDateTime(LocalDateTime.now());
         event.setStudy(study);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(), "'" + event.getTitle() + "' 모임을 만들었습니다."));
         return eventRepository.save(event);
     }
 
     public void updateEvent(final Event event, final EventForm eventForm) {
         modelMapper.map(eventForm, event);
         event.acceptWaitingList();
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(), "'" + event.getTitle() + "' 모임 정보를 수정했습니다."));
     }
 
     public void deleteEvent(final Event event) {
         eventRepository.delete(event);
+        eventPublisher.publishEvent(new StudyUpdateEvent(event.getStudy(), "'" + event.getTitle() + "' 모임을 취소했습니다."));
     }
 
     public void newEnrollment(final Event event, final Account account) {
